b52e42bf4e750091e5c4e0a4a59167767ebd9c09
milliax <wei.sivir.pw>
2021-08-30 20:45:22 +0800

    update

---
+++ d020_important/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define int long long
+
+using namespace std;
+
+int arr[100005];
+int sum[100005];
+
+set<int> s;
+
+signed main() {
+    int n, k;
+    scanf("%lld %lld", &n, &k);
+
+    s.insert(0);
+    int best = 0;
+    int v;
+    int sum = 0;
+    for (int i = 0; i < n; ++i) {
+        scanf("%lld", &v);
+        sum += v;
+        auto it = s.lower_bound(sum - k);
+        printf("sum - k %lld\n", sum - k);
+        printf("search %lld\n", *it);
+        if (it != s.end()) {
+            best = max(best, sum - *it);
+        }
+        s.insert(sum);
+    }
+    printf("%lld\n", best);
+
+    return 0;
+}
